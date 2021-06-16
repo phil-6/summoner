69e12cfa605e20725d342485973ee61131f5761b
Phil Reynolds <github@purpleriver.dev>
2021-06-16 08:03:06 +0100

    commit theme css and js files

---
+++ assets/js/themeToggle.js
@@ -0,0 +1,101 @@
+const themeBtn = document.querySelector("#theme-toggle");
+const colorBtn = document.querySelector("#color-switch");
+// check to see if OS preferences for light or dark mode
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+const prefersLightScheme = window.matchMedia("(prefers-color-scheme: light)");
+
+// check to see if local storage has a theme preference
+let currentTheme = localStorage.getItem("theme");
+let currentColor = localStorage.getItem( "color")
+let colors = ["orange", "red", "purple", "yellow", "green", "blue"]
+
+function setTheme() {
+    //if no local storage check against system preferences
+    if (currentTheme === null) {
+        if (prefersDarkScheme.matches) {
+            currentTheme = "dark"
+        } else if (prefersLightScheme.matches) {
+            currentTheme = "light"
+        } else {
+            // if no preferences, default to dark theme
+            currentTheme = "dark"
+        }
+        setTheme()
+    } else if (currentTheme === "dark") {
+        document.body.classList.remove("light");
+        document.body.classList.add("dark");
+    } else if (currentTheme === "light") {
+        document.body.classList.remove("dark");
+        document.body.classList.add("light");
+    }
+}
+
+function setColor() {
+    if (currentColor === "red") {
+        document.body.classList.remove(...colors)
+        document.body.classList.add("red");
+    }
+    if (currentColor === "orange") {
+        document.body.classList.remove(...colors)
+        document.body.classList.add("orange");
+    }
+    if (currentColor === "yellow") {
+        document.body.classList.remove(...colors)
+        document.body.classList.add("yellow");
+    }
+    if (currentColor === "green") {
+        document.body.classList.remove(...colors)
+        document.body.classList.add("green");
+    }
+    if (currentColor === "blue") {
+        document.body.classList.remove(...colors)
+        document.body.classList.add("blue");
+    }
+    if (currentColor === "purple") {
+        document.body.classList.remove(...colors)
+        document.body.classList.add("purple");
+    }
+}
+
+themeBtn.addEventListener("click", function () {
+    if (currentTheme === "dark") {
+        currentTheme = "light"
+        setTheme()
+    } else {
+        currentTheme = "dark";
+        setTheme()
+    }
+    console.log(currentTheme, currentColor)
+    localStorage.setItem("theme", currentTheme);
+});
+
+colorBtn.addEventListener("click", function () {
+    if (currentColor === "red") {
+        currentColor = "orange"
+        setColor()
+    } else if (currentColor === "orange") {
+        currentColor = "yellow"
+        setColor()
+    } else if (currentColor === "yellow") {
+        currentColor = "green"
+        setColor()
+    } else if (currentColor === "green") {
+        currentColor = "blue"
+        setColor()
+    } else if (currentColor === "blue") {
+        currentColor = "purple"
+        setColor()
+    } else if (currentColor === "purple") {
+        currentColor = "red"
+        setColor()
+    } else {
+        currentColor = "blue"
+        setColor()
+    }
+    console.log(currentTheme, currentColor)
+    localStorage.setItem("color", currentColor);
+});
+
+console.log(currentTheme, currentColor)
+setColor()
+setTheme()
