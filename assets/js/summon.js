fba59351a468d69d6c0c3117e7dd34c194a77618
phil-6 <github@purpleriver.dev>
2021-07-02 18:06:53 +0100

    begin work on share function

---
+++ assets/js/summon.js
@@ -0,0 +1,97 @@
+function summonThem() {
+    function formValidation(){
+        let validForm = true;
+        const phoneregex = /^(447\d{9})$/;
+        if (document.getElementById("summoner").value === ""){
+            document.getElementById("summoner").focus();
+            document.getElementById("summoner").classList.add("form-error");
+            document.getElementById("form-error-message").classList.remove("hide");
+            validForm = false;
+        }else{
+            document.getElementById("summoner").classList.remove("form-error");
+        }
+        if (document.getElementById("summonee").value === ""){
+            document.getElementById("summonee").focus();
+            document.getElementById("summonee").classList.add("form-error");
+            document.getElementById("form-error-message").classList.remove("hide");
+            validForm = false;
+        }else{
+            document.getElementById("summonee").classList.remove("form-error");
+        }
+        if (!phoneregex.test(document.getElementById("number").value)){
+            document.getElementById("number").focus();
+            document.getElementById("number").classList.add("form-error");
+            document.getElementById("form-error-message").classList.remove("hide");
+            validForm = false;
+        }else{
+            document.getElementById("number").classList.remove("form-error");
+        }
+        if ((document.getElementById("userLat").value === "")||(document.getElementById("userLng").value === "")){
+            document.getElementById("location-error").classList.remove("hide");
+            document.getElementById("form-container").classList.add("hide");
+            // document.body.classList.remove("is-main");
+            // document.body.classList.add("is-failure");
+            validForm = false;
+        }
+        if (validForm === true){
+            // document.getElementById("form-error-message").classList.add("d-none");
+            document.getElementById("form-container").classList.add("hide");
+            document.getElementById("loading-message").classList.remove("hide");
+            validSummon();
+        }
+    }
+
+
+
+    function validSummon() {
+        const jsonFormData = toJSONString(document.getElementById("summon-form"));
+        const API_GATEWAY_LOGIN_URL = "https://4ohtthv7a9.execute-api.eu-west-1.amazonaws.com/default";
+        const restful_action = "POST";
+
+        $.ajax({
+            type: restful_action,
+            cache: false,
+            data: jsonFormData,
+            contentType: "application/json",
+            url: API_GATEWAY_LOGIN_URL,
+            success: function (data) {
+                document.getElementById("loading-message").classList.add("d-none");
+                document.getElementById("success-content").innerHTML = (JSON.stringify(data).replace(/['"]+/g, ''));
+                document.getElementById("success-message").classList.remove("d-none");
+                document.body.classList.add("is-success");
+                return;
+            },
+            error: function (xhr) {
+                document.getElementById("general-error").classList.remove("d-none");
+                alert("An error occured: " + xhr.status + " " + xhr.statusText)
+            }
+        });
+    }
+    formValidation();
+}
+
+
+
+function testSummon() {
+    var philsData = JSON.stringify({"summoner-name":"testhardcodedsummoner","summonee-name":"testhardcoded","summonee-number":"447828170428","lat":"51.6639986","lng":"-3.9181857"});
+    // alert(philsData);
+    //console.log(philsData);
+    const API_GATEWAY_LOGIN_URL = "https://czanhrxz97.execute-api.eu-west-1.amazonaws.com/default";
+    var restful_action = "POST";
+
+    $.ajax({
+        type     : restful_action,
+        cache    : false,
+        data     : philsData,
+        contentType: "application/json",
+        url      : API_GATEWAY_LOGIN_URL,
+        success: function(data) {
+            alert(JSON.stringify(data));
+            return;
+        },
+        error    : function(xhr) {
+            alert("An error occured: " + xhr.status + " " + xhr.statusText)
+        }
+    });
+    // alert("after ajax" + philsData);
+}
