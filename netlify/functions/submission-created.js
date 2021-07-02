b68bda65c5889926e03b2dfeb32316a67fdbc5cc
phil-6 <github@purpleriver.dev>
2021-07-02 19:26:33 +0100

    test send sms 0

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,4 +1,39 @@
-// submission-created.js
-exports.handler = function(event, context, callback) {
-    console.log('Submission created!')
-};
+import TwilioSdk from 'twilio'
+// Your Account SID from www.twilio.com/console
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+// Your Auth Token from www.twilio.com/console
+const authToken = process.env.TWILIO_AUTH_TOKEN
+// instantiate twilio SDK
+const twilio = new TwilioSdk(accountSid, authToken)
+
+// use twilio SDK to send text message https://www.twilio.com/docs/libraries/node
+exports.handler = (event, context, callback) => {
+    const body = JSON.parse(event.body)
+
+    const sms = {
+        to: body.to,
+        body: body.message || '',
+        from: "The Summoner",
+    }
+
+    twilio.messages.create(sms).then((message) => {
+        console.log(`text message sent!`, message.body)
+        console.log(`date_created: ${message.date_created}`)
+        return callback(null, {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'Text message successfully sent!',
+                data: message,
+            })
+        })
+    }).catch((error) => {
+        console.log('text message failed: ', error)
+        return callback(null, {
+            statusCode: error.status,
+            body: JSON.stringify({
+                message: error.message,
+                error: error,
+            })
+        })
+    })
+}
