ab3307fc3f838c2368ca33c7237eda40563a031f
phil-6 <github@purpleriver.dev>
2021-07-02 19:36:26 +0100

    test send sms 2

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,11 +1,10 @@
-require('dotenv').config();
-
-const {
-    TWILIO_ACCOUNT_SID,
-    TWILIO_AUTH_TOKEN
-} = process.env;
-
-const twilio = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+import TwilioSdk from 'twilio'
+// Your Account SID from www.twilio.com/console
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+// Your Auth Token from www.twilio.com/console
+const authToken = process.env.TWILIO_AUTH_TOKEN
+// instantiate twilio SDK
+const twilio = new TwilioSdk(accountSid, authToken)
 
 // use twilio SDK to send text message https://www.twilio.com/docs/libraries/node
 exports.handler = (event, context, callback) => {
